5cee7bbc1b422b12905bb063283960a129540389
Matteo Gildone <44809484+mgildone@users.noreply.github.com>
2019-05-28 15:46:50 +0100

    first commit

---
+++ src/data/races/index.js
@@ -0,0 +1,28 @@
+const races = [
+  {
+    name: "Human",
+    pct: 90,
+    abbreviation: "human"
+  },
+  {
+    name: "Dwarf",
+    pct: 5,
+    abbreviation: "dwarf"
+  },
+  {
+    name: "Halfling",
+    pct: 3,
+    abbreviation: "halfling"
+  },
+  {
+    name: "High Elf",
+    pct: 1,
+    abbreviation: "elf"
+  },
+  {
+    name: "Wood Elf",
+    pct: 1,
+    abbreviation: "elf"
+  }
+];
+export default races;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import generateCharacter from "./generateCharacter";
 
 import "./styles.css";
 
+console.log(JSON.stringify(generateCharacter));
+
 function App() {
+  const [character] = useState(generateCharacter);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!!!</h2>
+      <p>
+        Hello {character.characterFirstName} {character.characterLastName}, you
+        are a {character.characterRace.name} {character.characterGender.name}
+      </p>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App character={generateCharacter} />, rootElement);
