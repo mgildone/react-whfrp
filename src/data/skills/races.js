030432d229e26575fe3f91320594e603bc70d15b
Matteo Gildone <matteo.gildone@outlook.com>
2019-06-02 21:08:30 +0100

    skills and talents by race

---
+++ src/data/skills/races.js
@@ -0,0 +1,72 @@
+const human = [
+  "Animal Care",
+  "Charm",
+  "Cool",
+  "Evaluate",
+  "Gossip",
+  "Haggle",
+  "Language (Bretonnian)",
+  "Language (Wastelander)",
+  "Leadership",
+  "Lore (Reikland)",
+  "Melee (Basic)",
+  "Ranged (Bow)"
+];
+const dwarf = [
+  "Consume Alcohol",
+  "Cool",
+  "Endurance",
+  "Entertain (Storytelling)",
+  "Evaluate",
+  "Intimidate",
+  "Language (Khazalid)",
+  "Lore (Dwarfs)",
+  "Lore (Geology)",
+  "Lore (Metallurgy)",
+  "Melee (Basic)",
+  "Trade (Any one)"
+];
+const halfling = [
+  "Charm",
+  "Consume Alcohol",
+  "Dodge",
+  "Gamble",
+  "Haggle",
+  "Intuition",
+  "Language (Mootish)",
+  "Lore (Reikland)",
+  "Perception",
+  "Sleight of Hand",
+  "Stealth (Any one)",
+  "Trade (Cook)"
+];
+const helf = [
+  "Cool",
+  "Entertain (Sing)",
+  "Evaluate",
+  "Language (Eltharin)",
+  "Leadership",
+  "Melee (Basic)",
+  "Navigation",
+  "Perception",
+  "Play (Any one)",
+  "Ranged (Bow)",
+  "Sail",
+  "Swim"
+];
+const welf = [
+  "Athletics",
+  "Climb",
+  "Endurance",
+  "Entertain (Sing)",
+  "Intimidate",
+  "Language (Eltharin)",
+  "Melee (Basic)",
+  "Outdoor Survival",
+  "Perception",
+  "Ranged (Bow)",
+  "Stealth (Rural)",
+  "Track"
+];
+
+export default { human, dwarf, halfling, helf, welf };
---
+++ src/data/talents/races.js
@@ -0,0 +1,32 @@
+const human = ["Doomed", "Savvy or Suave", "Random", "Random", "Random"];
+const dwarf = [
+  "Magic Resistance",
+  "Night Vision",
+  "Read/Write or Relentless",
+  "Resolute or Strong-Minded",
+  "Sturdy"
+];
+const halfling = [
+  "Acute Sense (Taste)",
+  "Night Vision",
+  "Resistance (Chaos)",
+  "Small",
+  "Random",
+  "Random"
+];
+const helf = [
+  "Acute Sense (Vision)",
+  "Coolheaded or Savvy",
+  "Night Vision",
+  "Second Sight or Sixth Sense",
+  "Read/Write"
+];
+const welf = [
+  "Acute Sense (Vision)",
+  "Hardy or Second Sight",
+  "Night Vision",
+  "Read/Write or Very Resilient",
+  "Rover"
+];
+
+export default { human, dwarf, halfling, helf, welf };
